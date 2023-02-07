751ff1a0a72b4e61a10d0789ad28b4852d80a134
AhmedKhaled146 <a7med74yaso@gmail.com>
2023-02-07 15:11:12 +0200

    add some feture

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,6 +2,7 @@ class TodosController < ApplicationController
 
     def index
         @todo = Todo.all
+        @todos = Todo.search(params[:search])
     end
 
     def new
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,9 @@
 class Todo < ApplicationRecord
-    
+    def self.search(search)
+        if search
+            where(["name LIKE ?", "%#{search}%"])
+        else
+            all
+        end
+    end
 end
