9f04129383b9a987861c4abe22ecda10bdb446b2
AhmedKhaled146 <a7med74yaso@gmail.com>
2023-03-10 02:36:09 +0200

    add json at controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -9,36 +9,46 @@ class TodosController < ApplicationController
         @todo = Todo.new
     end
 
-    def edit
+    def create
+        @todo = Todo.new(todo_params)
+        respond_to do |format|
+            if @todo.save
+                format.html {redirect_to @todo, notice: "#{@todo.title} was successfully created."}
+                format.json { render :show, status: :created, location: @user }
+            else
+                format.html { render :new, status: :unprocessable_entity }
+                format.json { render json: @todo.errors, status: :unprocessable_entity }
+            end
+        end
     end
 
     def show
     end
 
-    def destroy
-        @todo.destroy
-        redirect_to root_path
-    end
-
-
-    def create
-        @todo = Todo.new(todo_params)
-        respond_to do |format|
-            if @todo.save
-                format.html {redirect_to root_path}
-            end
-        end
+    def edit
     end
 
     def update
         @todo.update(todo_params)
         respond_to do |format|
             if @todo.save
-                format.html {redirect_to root_path}
+                format.html {redirect_to @todo, notice: "#{@todo.title} was successfully Updated."}
+                format.json { render :show, status: :ok, location: @user }
+            else
+                format.html { render :edit, status: :unprocessable_entity }
+                format.json { render json: @todo.errors, status: :unprocessable_entity }
             end
         end
     end
-    
+
+    def destroy
+        @todo.destroy
+        respond_to do |format|
+            format.html { redirect_to root_path, notice: "User was successfully destroyed." }
+            format.json { head :no_content }
+        end
+    end
+
     private
 
     def set_todo
